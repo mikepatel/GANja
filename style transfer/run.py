991fb837674ee12fc2a5bb55e01626526e31ca7f
Michael Patel <michael.patel94@gmail.com>
2020-11-10 13:11:41 -0500

    Adding preprocess function

--- style transfer/run.py
+++ style transfer/run.py
@@ -28,6 +28,37 @@ DATA_DIR = os.path.join(os.getcwd(), "data")
 
 
 ################################################################################
+# load image
+def load_image(filename, mode):
+    data_subdirectory = os.path.join(DATA_DIR, mode)
+    filepath = os.path.join(data_subdirectory, filename)
+    image = Image.open(filepath)
+    return image
+
+
+# preprocess image
+def preprocess_image(filename, mode="content"):
+    image = load_image(filename, mode)
+
+    if mode is "style":
+        # style image size should be 256x256
+        # content image size can be arbitrary
+        image = image.resize((256, 256))
+
+    # image to array
+    image = tf.keras.preprocessing.image.img_to_array(image)
+
+    # normalize image: [0, 255] --> [0, 1]
+    image = image / 255.0
+
+    # reshape: (1, WIDTH, HEIGHT, CHANNELS)
+    # currently, batch size = 1
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+
+################################################################################
 # Main
 if __name__ == "__main__":
     # environment setup
@@ -39,44 +70,27 @@ if __name__ == "__main__":
     # ETL = Extraction, Transformation, Load
     # Content image
     content_image_filename = "content.jpg"
-    content_image_filepath = os.path.join(DATA_DIR, content_image_filename)
-    content_image = Image.open(content_image_filepath)
-
-    # resize, content image size can be arbitrary
-    #content_image = content_image.resize((700, 500))
-
-    # image to array
-    content_image = tf.keras.preprocessing.image.img_to_array(content_image)
-
-    # normalize image
-    content_image = content_image / 255.0
-
-    # reshape: (1, WIDTH, HEIGHT, CHANNELS)
-    content_image = np.expand_dims(content_image, axis=0)
+    content_image = preprocess_image(
+        filename=content_image_filename,
+        mode="content"
+    )
 
     # Style image
     style_image_filename = "style.jpg"
-    style_image_filepath = os.path.join(DATA_DIR, style_image_filename)
-    style_image = Image.open(style_image_filepath)
-
-    # style image size should be 256x256
-    style_image = style_image.resize((256, 256))
-
-    # image to array
-    style_image = tf.keras.preprocessing.image.img_to_array(style_image)
-
-    # normalize image
-    style_image = style_image / 255.0
-
-    # reshape: (1, WIDTH, HEIGHT, CHANNELS)
-    style_image = np.expand_dims(style_image, axis=0)
+    style_image = preprocess_image(
+        filename=style_image_filename,
+        mode="style"
+    )
 
     # ---- MODEL ---- #
     hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
     hub_module = hub.load(hub_handle)
 
     # ----- GENERATE ----- #
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+    outputs = hub_module(
+        tf.constant(content_image),
+        tf.constant(style_image)
+    )
     #print(f'Num outputs: {len(outputs)}')
     output_image = outputs[0]
 
@@ -88,6 +102,9 @@ if __name__ == "__main__":
     output_image = tf.keras.preprocessing.image.array_to_img(output_image)
     #print(output_image)
     #output_image.show()
+
+    # save generated image
     output_image_filename = "generated_" + datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".jpg"
-    output_image_filepath = os.path.join(DATA_DIR, output_image_filename)
+    output_image_subdirectory = os.path.join(DATA_DIR, "generated")
+    output_image_filepath = os.path.join(output_image_subdirectory, output_image_filename)
     output_image.save(output_image_filepath)
