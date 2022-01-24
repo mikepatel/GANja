4f1008bd8b91c46a559acb52c36883fdc54b8a8a
Michael <michael.patel94@gmail.com>
2022-01-24 17:40:59 -0500

    Reorganizing style_transfer/ directory

---
+++ style transfer/static/upload.js
@@ -0,0 +1,31 @@
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#blah')
+				.attr('src', e.target.result)
+				.width(150)
+				.height(200);
+		};
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+
+function readURL2(input){
+	if(input.files && input.files[0]){
+		var reader = new FileReader();
+		
+		reader.onload = function (e) {
+			$("#blah2")
+				.attr('src', e.target.result)
+				.width(150)
+				.height(200);
+		};
+		
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
